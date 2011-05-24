397b704156eb55ddc6fc2a5e3830bc4d823b5b91
Gaetan Bailleul <gaetan.bailleul@gmail.com>
2011-05-24 08:33:21 +0200

    ajout complet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybacasable::Application.config.secret_token = '5dd121e6e7e0227e1523d25309abefdc30243f0a03ef862491fd618206d9ccbc6f476ec7c012765baa8527785d640da2cd6bcc3ba9ecc93709a12a321f14117c'
